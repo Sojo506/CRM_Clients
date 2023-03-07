630940353db7d57125fa651c8fc9e5294d1a142d
Sojo506 <fabiansojowork@gmail.com>
2023-03-07 15:27:36 -0600

    validation to create new customers

--- js/newCustomer.js
+++ js/newCustomer.js
@@ -1,4 +1,30 @@
 (function () {
-  const form = document.querySelector('#formulario')
+  const form = document.querySelector("#form");
+  form.addEventListener("submit", validateCustomer);
 
+  function validateCustomer(e) {
+    e.preventDefault();
+
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const phone = document.querySelector("#phone").value;
+    const company = document.querySelector("#company").value;
+
+    const customer = {
+      name,
+      email,
+      phone,
+      company,
+    };
+
+    if (validate(customer)) {
+      // show message
+      console.log('Campos requeridos')
+      return 
+    }
+  }
+
+  function validate(obj) {
+    return !Object.values(obj).every((input) => input !== "");
+  }
 })();
