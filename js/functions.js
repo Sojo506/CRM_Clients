e5a020a66ef5c6925cb198d3ef50062b925b1164
Sojo506 <fabiansojowork@gmail.com>
2023-03-09 10:31:11 -0600

    add(js/functions.js): functions to show an alert

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+export function showAlert(message, type) {
+  const alert = document.querySelector(".bg-red-100");
+
+  if (!alert) {
+    const alert = document.createElement("P");
+    alert.classList.add(
+      `bg-${type === "error" ? "red" : "green"}-100`,
+      `border-${type === "error" ? "red" : "green"}-400`,
+      "text-red-700",
+      "px-4",
+      "py-3",
+      "rounded",
+      "max-w-lg",
+      "mx-auto",
+      "mt-6",
+      "text-center"
+    );
+
+    alert.innerHTML = `
+      <strong class="font-bold">${type === "error" ? "Error" : "Done"}!</strong>
+      <span class="block sm:inline">${message}</span>
+    `;
+
+    const form = document.querySelector("#form");
+    form.appendChild(alert);
+
+    setTimeout(() => {
+      alert.remove();
+    }, 3000);
+  }
+}
